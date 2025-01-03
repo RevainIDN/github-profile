f0a034dd8cc9d44dfacb05c49598f5d9c0a357f9
RevainIDN <kov.igor94@gmail.com>
2025-01-03 19:00:06 +0200

    Незначительное улучшение отображение информации

--- src/components/RepositoryCard.tsx
+++ src/components/RepositoryCard.tsx
@@ -1,5 +1,11 @@
 import '../styles/RepositoryCard.css'
 
+interface LicenseInfo {
+	key: string;
+	name: string;
+	spdx_id: string;
+}
+
 interface RepositoryUserFiltered {
 	name: string;
 	description: string | null;
@@ -7,13 +13,15 @@ interface RepositoryUserFiltered {
 	stargazers_count: number;
 	updated_at: string;
 	html_url: string;
+	license: LicenseInfo | null;
 }
 
 interface RepositoryUserProps {
 	userRepository: RepositoryUserFiltered[] | null;
+	renderAllRepositories: (userRepository: RepositoryUserFiltered[]) => RepositoryUserFiltered[];
 }
 
-export default function RepositoryCard({ userRepository }: RepositoryUserProps) {
+export default function RepositoryCard({ userRepository, renderAllRepositories }: RepositoryUserProps) {
 	const formatDate = (date: string) => {
 		const updateDate = new Date(date);
 		const currentDate = new Date();
@@ -32,12 +40,20 @@ export default function RepositoryCard({ userRepository }: RepositoryUserProps) 
 	return (
 		<>
 			{userRepository ? (
-				userRepository.map((repository, index) => (
-					<a href={repository.html_url} target="_blank" rel="noopener noreferrer">
-						<div key={index} className='repository-cont'>
+				renderAllRepositories(userRepository).map((repository, index) => (
+					<a key={index} href={repository.html_url} target="_blank" rel="noopener noreferrer">
+						<div className='repository-cont'>
 							<h1 className='repository-name'>{repository.name}</h1>
 							<p className='repository-desc'>{repository.description}</p>
 							<div className='repository-statistics'>
+								{repository.license && repository.license.name !== null ? (
+									<div className='repository-statistic'>
+										<img className='repository-statistic-icon' src="Chield_alt.svg" alt="" />
+										<p className='repository-count'>MIT</p>
+									</div>
+								) : (
+									null
+								)}
 								<div className='repository-statistic'>
 									<img className='repository-statistic-icon' src="Nesting.svg" alt="" />
 									<p className='repository-count'>{repository.forks}</p>
--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -24,12 +24,12 @@ export default function UserInfo({ userInfo }: InfoUserProps) {
 				<ul className='info-list'>
 					<li className='info-item'><span>Followers</span>{userInfo.followers}</li>
 					<li className='info-item'><span>Following</span>{userInfo.following}</li>
-					<li className='info-item'><span>Location</span>{userInfo.location}</li>
+					<li className='info-item'><span>Location</span>{userInfo.location !== null ? userInfo.location : 'Location unknown'}</li>
 				</ul>
 			</div>
 			<div className='info-title-cont'>
 				<h1 className='info-name'>{userInfo.name}</h1>
-				<p className='info-description'>{userInfo.bio}</p>
+				<p className='info-description'>{userInfo.bio !== null ? userInfo.bio : 'User description missing :('}</p>
 			</div>
 		</div>
 
